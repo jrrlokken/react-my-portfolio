9f30e1be5027fc97771f22490110b1935517bdc4
Joshua Lokken <joshualokken@pm.me>
2021-09-03 08:40:02 -0500

    Updated svg on About page

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,12 @@ import React from "react";
 export default function About() {
   return (
     <section id="about">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+      <div className="container flex flex-col items-center px-10 py-20 mx-auto md:flex-row">
+        <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0">
+          <h1 className="mb-4 text-3xl font-medium text-white title-font sm:text-4xl">
             Hi, I'm Joshua.
           </h1>
-          <h2 className="title-font sm:text-3xl text-2xl mb-4 font-medium text-white">
+          <h2 className="mb-4 text-2xl font-medium text-white title-font sm:text-3xl">
             I love to build amazing apps.
           </h2>
           <p className="mb-8 leading-relaxed">
@@ -19,19 +19,19 @@ export default function About() {
           <div className="flex justify-center">
             <a
               href="#contact"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+              className="inline-flex px-6 py-2 text-lg text-white bg-green-500 border-0 rounded focus:outline-none hover:bg-gray-700 hover:text-white"
             >
               Work With Me
             </a>
             <a
               href="#projects"
-              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+              className="inline-flex px-6 py-2 ml-4 text-lg text-gray-400 bg-gray-800 border-0 rounded focus:outline-none hover:bg-gray-700 hover:text-white"
             >
               See My Past Work
             </a>
           </div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <div className="w-5/6 lg:max-w-lg lg:w-full md:w-1/2">
           <img
             className="object-cover object-center rounded"
             alt="hero"
