cf5560d40967192d58f6fcd2140b4710f641f5ac
Joshua Lokken <joshualokken@pm.me>
2021-09-14 14:40:00 -0500

    Reversed the title and subtitle divs for better readability

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,11 +7,11 @@ export default function Projects() {
     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
-          <CodeIcon className="mx-auto inline-block w-10 mb-4" />
-          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+          <CodeIcon className="inline-block w-10 mx-auto mb-4" />
+          <h1 className="mb-4 text-3xl font-medium text-white sm:text-4xl title-font">
             Apps I've Built
           </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+          <p className="mx-auto text-base leading-relaxed lg:w-2/3">
             Here are some of the projects that best showcase my skills. All of
             my code is hosted on Github, and there are many other projects
             there.
@@ -22,23 +22,23 @@ export default function Projects() {
             <a
               href={project.link}
               key={project.image}
-              className="sm:w-1/2 w-100 p-4"
+              className="p-4 sm:w-1/2 w-100"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <div className="flex relative">
+              <div className="relative flex">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute inset-0 object-cover object-center w-full h-full"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                    {project.subtitle}
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
+                <div className="relative z-10 w-full px-8 py-10 bg-gray-900 border-4 border-gray-800 opacity-0 hover:opacity-100">
+                  <h1 className="mb-1 text-lg font-medium text-white title-font">
                     {project.title}
                   </h1>
+                  <h2 className="mb-3 text-sm font-medium tracking-widest text-green-400 title-font">
+                    {project.subtitle}
+                  </h2>
                   <p className="leading-relaxed">{project.description}</p>
                 </div>
               </div>
