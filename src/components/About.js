c054be77c6dfaa5c6798a9fd31511ce9c25c8d2b
Joshua Lokken <joshualokken@pm.me>
2021-09-17 12:57:18 -0500

    Changed button text in About component

--- src/components/About.js
+++ src/components/About.js
@@ -21,13 +21,13 @@ export default function About() {
               href="#contact"
               className="inline-flex px-6 py-2 text-lg text-white bg-green-500 border-0 rounded focus:outline-none hover:bg-gray-700 hover:text-white"
             >
-              Work With Me
+              Contact
             </a>
             <a
               href="#projects"
               className="inline-flex px-6 py-2 ml-4 text-lg text-gray-400 bg-gray-800 border-0 rounded focus:outline-none hover:bg-gray-700 hover:text-white"
             >
-              See My Past Work
+              Projects
             </a>
           </div>
         </div>
