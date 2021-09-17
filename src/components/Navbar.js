964e682debe3ac73ffb8df71770ff83de58ea58f
Joshua Lokken <joshualokken@pm.me>
2021-09-17 12:57:46 -0500

    Added button to view resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import { ArrowRightIcon } from "@heroicons/react/solid";
 import React from "react";
+import Resume from "./Resume";
 
 export default function Navbar() {
   return (
     <header className="top-0 z-10 bg-gray-800 md:sticky">
       <div className="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
         <div className="mb-4 font-medium text-white title-font md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
+          <a href="#about" className="ml-3 text-2xl">
             Joshua Lokken
           </a>
         </div>
@@ -17,21 +18,19 @@ export default function Navbar() {
           <a href="#skills" className="mr-5 hover:text-white">
             Skills
           </a>
-          <a
+          {/* <a
             href="https://drive.google.com/file/d/1n7NkMw8VqclIcWj_i3XjK4au4WMS1w0R/view?usp=sharing"
             className="mr-5 hover:text-white"
             target="_blank"
             rel="noopener noreferrer"
           >
             Resume
-          </a>
-          {/* <a href="#testimonials" className="mr-5 hover:text-white">
-            Testimonials
           </a> */}
+          <Resume />
         </nav>
         <a
           href="#contact"
-          className="inline-flex items-center px-3 py-1 mt-4 text-base bg-gray-800 border-0 rounded focus:outline-none hover:bg-gray-700 md:mt-0"
+          className="inline-flex items-center px-3 py-1 mt-4 text-base bg-gray-800 border-0 rounded focus:outline-none hover:bg-gray-700 md:mt-0 hover:text-white"
         >
           Hire Me
           <ArrowRightIcon className="w-4 h-4 ml-1" />
