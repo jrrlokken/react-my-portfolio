8a2e63fb49f77863450855ae31396fbf1b1ef7c8
Joshua Lokken <joshualokken@pm.me>
2021-09-22 08:38:56 -0500

    Added Resume component to view pdf resume, added pdf resume

---
+++ src/components/Resume.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { PDFViewer } from "react-view-pdf";
+
+import myResume from "./resume.pdf";
+
+export default function Resume() {
+  const [shown, setShown] = useState(false);
+  const resume = myResume;
+
+  // function onDocumentLoadSuccess({ numPages }) {
+  //   setNumPages(numPages);
+  //   setPageNumber(1);
+  // }
+
+  // function changePage(offset) {
+  //   setPageNumber((prevPageNumber) => prevPageNumber + offset);
+  // }
+
+  // function previousPage() {
+  //   changePage(-1);
+  // }
+
+  // function nextPage() {
+  //   changePage(1);
+  // }
+
+  const modalBody = () => (
+    <div
+      style={{
+        backgroundColor: "#fff",
+        flexDirection: "column",
+        overflow: "hidden",
+
+        /* Fixed position */
+        left: 0,
+        position: "fixed",
+        top: 0,
+
+        /* Full window size */
+        height: "100%",
+        width: "100%",
+
+        /* Display on top of other elements */
+        zIndex: 99,
+      }}
+    >
+      <div
+        style={{
+          alignItems: "center",
+          backgroundColor: "#000",
+          color: "#fff",
+          display: "flex",
+          padding: ".5rem",
+        }}
+      >
+        <button
+          style={{
+            backgroundColor: "#357edd",
+            border: "none",
+            borderRadius: "4px",
+            color: "#ffffff",
+            cursor: "pointer",
+            padding: "8px",
+          }}
+          onClick={() => setShown(false)}
+        >
+          Close
+        </button>
+      </div>
+      <div>
+        <PDFViewer url={resume} />
+      </div>
+    </div>
+  );
+
+  return (
+    <div>
+      <button
+        style={{
+          backgroundColor: "#00449e",
+          border: "none",
+          borderRadius: ".25rem",
+          color: "#fff",
+          cursor: "pointer",
+          padding: ".5rem",
+        }}
+        onClick={() => setShown(true)}
+      >
+        Resume
+      </button>
+      {shown && ReactDOM.createPortal(modalBody(), document.body)}
+    </div>
+  );
+}
