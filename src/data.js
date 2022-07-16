7b6655cb67d0a37f4bc11cf88dc828b5d2413c3a
Joshua Lokken <joshualokken@pm.me>
2022-07-15 20:47:05 -0500

    Update skills, projects

--- src/data.js
+++ src/data.js
@@ -1,47 +1,51 @@
 export const projects = [
   {
-    title: "Issue Tracking System",
-    subtitle: "Python and Flask",
+    title: "Lokken Reunion",
+    subtitle: "Node.js and Express, EJS, and MongoDB",
     description:
-      "My first Python project.  Flask frontend, WTForms, REST API, PostgreSQL database, testing with Selenium and unittest.",
-    image: "./project01.gif",
-    link: "https://flask-its.herokuapp.com",
+      "Server rendered family reunion nostalgia website. Includes live chat/commenting.",
+    image: "./lokkenreunion.gif",
+    link: "https://lokken-reunion.herokuapp.com/",
+    repo: "https://github.com/jrrlokken/reunion",
+  },
+  {
+    title: "DocuCode",
+    subtitle: "Node.js CLI and React",
+    description:
+      "A CLI with React frontend for generating documentation, with live code editor and markdown editor.",
+    image: "./docucode.gif",
+    link: "https://docucode.joshualokken.tech",
+    repo: "https://github.com/jrrlokken/docucode",
   },
   {
     title: "Mask Up!",
     subtitle: "Keystone headless CMS and NextJS",
     description:
       "A facemask marketplace built on Keystone's headless CMS with NextJS.",
-    image: "./project02.gif",
+    image: "./maskup.gif",
     link: "https://maskup.joshualokken.tech",
-  },
-  {
-    title: "Tasklist",
-    subtitle: "React hooks and context API.",
-    description: "Todo app with React hooks and context API.",
-    image: "./project03.gif",
-    link: "https://tasklist.joshualokken.tech",
   },
   {
     title: "PaletteSwatch",
     subtitle: "React Color Palette Generator",
     description:
       "Color palette generator in React. Implements drag and drop color tiles.",
-    image: "./project04.gif",
+    image: "./paletteswatch.gif",
     link: "https://paletteswatch.joshualokken.tech",
   },
   {
-    title: "ME*N stack server",
-    subtitle: "Node and Express",
-    description: "Node server with Express authentication",
-    image: "./project05.png",
-    link: "https://github.com/jrrlokken/node-auth",
+    title: "Issue Tracking System",
+    subtitle: "Python and Flask",
+    description:
+      "My first Python project.  Flask frontend, WTForms, REST API, PostgreSQL database, testing with Selenium and unittest.",
+    image: "./its.gif",
+    link: "https://flask-its.herokuapp.com",
   },
   {
     title: "My Old Portfolio",
     subtitle: "HTML, CSS and jQuery",
     description: "A first attempt at a handrolled portfolio",
-    image: "./project06.gif",
+    image: "./oldportfolio.gif",
     link: "https://old-portfolio.joshualokken.tech",
   },
 ];
@@ -50,18 +54,15 @@ export const testimonials = [];
 
 export const skills = [
   "JavaScript",
+  "HTML",
+  "CSS",
+  "Node.js",
   "React",
+  "Linux",
   "Next.js",
-  "Node",
-  "Python",
-  "Flask",
   "SQL/NoSQL",
   "Responsive Web Design",
   "Software Development Lifecycle",
-  "Linux",
-  "Containerization",
-  "Docker",
   "Vercel",
   "Heroku",
-  "Netlify",
 ];
