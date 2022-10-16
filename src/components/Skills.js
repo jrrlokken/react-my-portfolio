8c57ea076ed402284107f61e995c2540eaf4f5a0
Joshua Lokken <joshualokken@pm.me>
2022-10-16 06:46:57 -0500

    Update skills, resume

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,28 +1,28 @@
-import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
-import React from "react";
-import { skills } from "../data";
+import { BadgeCheckIcon, ChipIcon } from '@heroicons/react/solid';
+import React from 'react';
+import { skills } from '../data';
 
 export default function Skills() {
   return (
-    <section id="skills">
-      <div className="container px-5 py-10 mx-auto">
-        <div className="text-center mb-20">
-          <ChipIcon className="w-10 inline-block mb-4" />
-          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+    <section id='skills'>
+      <div className='container px-5 py-10 mx-auto'>
+        <div className='mb-20 text-center'>
+          <ChipIcon className='inline-block w-10 mb-4' />
+          <h1 className='mb-4 text-3xl font-medium text-white sm:text-4xl title-font'>
             Skills &amp; Technologies
           </h1>
-          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+          <p className='mx-auto text-base leading-relaxed xl:w-2/4 lg:w-3/4'>
             These are the tools that I've spent the most time working with. I
             have used other tools and frameworks, but these are my favorites
             thus far.
           </p>
         </div>
-        <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+        <div className='flex flex-wrap -mx-2 lg:w-4/5 sm:mx-auto sm:mb-2'>
           {skills.map((skill) => (
-            <div key={skill} className="p-2 sm:w-1/2 w-full">
-              <div className="bg-gray-800 rounded flex p-4 h-full items-center">
-                <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
-                <span className="title-font font-medium text-white">
+            <div key={skill} className='w-full p-2 sm:w-1/2'>
+              <div className='flex items-center h-full p-4 bg-gray-800 rounded'>
+                <BadgeCheckIcon className='flex-shrink-0 w-6 h-6 mr-4 text-green-400' />
+                <span className='font-medium text-white title-font'>
                   {skill}
                 </span>
               </div>
